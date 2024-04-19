06705ecb3bcb90e766f00a344032317c950e5f4a
Victor Korir <vkkorir@gmail.com>
2024-04-18 23:18:03 -0700

    updated project settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#003262',
+        secondary: '#3b7ea1',
+        warning: '#ed4e33'
+      }
+    },
   },
   plugins: [],
 }
