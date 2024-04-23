58ec88eed356bb11365715c956ee839b2b12dbb1
Victor Korir <vkkorir@gmail.com>
2024-04-23 14:09:54 -0700

    Added a function to set user claims on each access

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,52 +1,114 @@
 import { initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
+import { error, info, warn } from 'firebase-functions/logger';
+import { onCall } from 'firebase-functions/v2/https';
 import {
   HttpsError,
   beforeUserCreated,
   beforeUserSignedIn,
 } from 'firebase-functions/v2/identity';
 
-const app = initializeApp();
-const allowedDomain = '@berkeley.edu';
+initializeApp();
+const DOMAIN = '@berkeley.edu';
+const COL_USERS = 'users';
+const ROLE_ADMIN = 'ADMIN';
+const ROLE_STUDENT = 'STUDENT';
 
-// Restrict domain, and check if user has access
-exports.onCreate = beforeUserCreated(async (event) => {
+// Check authorization & create user
+export const onCreate = beforeUserCreated(async (event) => {
   const email = event.data.email;
+  const uid = event.data.uid;
+  const name = event.data.displayName;
+
+  info(`creating user uid:${uid}, email:${email}`);
+
   if (!email) {
+    error('invalid email');
     throw new HttpsError('invalid-argument', 'Invalid email argument');
   }
-  if (!email.includes(allowedDomain)) {
+  if (!email.includes(DOMAIN)) {
+    error('Non berkeley domain account ');
     throw new HttpsError('permission-denied', 'User your berkeley email');
   }
-  if ((await getUserSnapshot(email)).size == 0) {
-    throw new HttpsError('not-found', 'User has no access');
+  const authorized = await getFirestore()
+    .collection(COL_USERS)
+    .doc(email)
+    .get();
+  if (!authorized.exists) {
+    warn('user not authorized, or already registered');
+    throw new HttpsError('not-found', 'User not authorized');
   }
-});
-
-// Set user custom claims from role
-exports.onSignIn = beforeUserSignedIn(async (event) => {
-  const email = event.data.email;
-  if (!email) {
-    throw new HttpsError('invalid-argument', 'Invalid email');
-  }
-  const snapshot = await getUserSnapshot(email);
-  snapshot.forEach(async (_user) => {
-    const data = _user.data();
-    let claims = {};
-    if (data.role == 'ADMIN') {
-      claims = { admin: true };
-    }
-    if (data.role == 'STUDENT') {
-      claims = { student: true };
-    }
-    await getAuth().setCustomUserClaims(event.data?.uid, claims);
+  // create user from admin provided attributes
+  const ref = getFirestore().collection(COL_USERS).doc(uid);
+  await ref.set({
+    name,
+    email,
+    createdAt: Date.now(),
+    accessCount: 0,
+    ...authorized.data(),
   });
+  // clean up no-longer needed attributes
+  await getFirestore().collection(COL_USERS).doc(email).delete();
+
+  info('user created successfully');
 });
 
-const getUserSnapshot = async (email: string) => {
-  const firestore = getFirestore(app);
-  const usersCol = firestore.collection('users');
-  const query = usersCol.where('email', '==', email);
-  return await query.get();
-};
+// Update login data
+export const onSignIn = beforeUserSignedIn(async (event) => {
+  const email = event.data.email;
+  const uid = event.data.uid;
+
+  info(`signing in user uid:${uid}, email:${email}`);
+
+  if (!email || !uid) {
+    error('invalid email or uid');
+    throw new HttpsError('invalid-argument', 'Invalid email or uid');
+  }
+
+  const ref = getFirestore().collection(COL_USERS).doc(uid);
+  const snapshot = await ref.get();
+  if (!snapshot.exists) {
+    error('user does not exist.');
+    throw new HttpsError('not-found', 'User not found');
+  }
+
+  const data = snapshot.data();
+  ref.update({
+    accessCount: data?.accessCount + 1,
+    lastAccessAt: Date.now(),
+  });
+
+  info('user signed in successfully.');
+});
+
+// Update claims
+export const setClaims = onCall(async (request) => {
+  const uid = request.auth?.uid;
+
+  info(`setting custom claims for ${uid}`);
+  if (!uid) {
+    error('invalid uid');
+    return null;
+  }
+
+  const ref = getFirestore().collection(COL_USERS).doc(uid);
+  const snapshot = await ref.get();
+  if (!snapshot.exists) {
+    error('user does not exist.');
+    return null;
+  }
+
+  const data = snapshot.data();
+  let claims: object | null = null;
+  if (data && data.role == ROLE_ADMIN) {
+    claims = { admin: true };
+  } else if (data && data.role == ROLE_STUDENT) {
+    claims = { student: true };
+  }
+
+  await getAuth().setCustomUserClaims(uid, claims);
+  info(`custom claims set successfully with role ${data?.role}`);
+
+  return claims;
+});
